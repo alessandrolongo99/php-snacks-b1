62a50794a7798210f14cc7d01a40a0c3c0976021
alessandrolongo99 <longoalessandro153@gmail.com>
2022-08-23 17:33:14 +0200

    snack 2

--- index.php
+++ index.php
@@ -63,6 +63,29 @@ $games = [
         ?>
     </ul>
 
+    <!-- **************************** -->
+
+    <h2>Snack 2</h2>
+    <p>Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione)
+        che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
+        Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato"</p>
+
+    <?php
+        $name = $_GET['name'];
+        $age = $_GET['age'];
+        $mail = $_GET['mail'];
+
+        echo '<h4>Nome: '.$name.'</h4>';
+        echo '<h4>Eta: '.$age.'</h4>';
+        echo '<h4>Mail: '.$mail.'</h4>';
+
+        if ((strlen($name) > 3) && (is_numeric($age)) && ((strpos($mail, '@') > 0) && (strpos($mail, '.') > 3))){
+            echo '<h4>Accesso riuscito</h4>';
+        }else{
+            echo '<h4>Accesso negato</h4>';
+        }
+    ?>
+
 </body>
 
 </html>
